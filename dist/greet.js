298fb9fc80f4d04b6abdec39b7e8f0971a34b90e
Ngomso Gaya <ngomso.gaya>
2024-04-22 09:05:34 +0200

    node modules and transcribed js files

--- dist/greet.js
+++ dist/greet.js
@@ -1,17 +1,16 @@
-import Person from "./person";
-import { language } from "./language";
-import { GreetIn } from "./greetIn";
-import { GreetInXhosa } from "./greetIn";
-import { GreetInEnglish } from "./greetIn";
-import { GreetInAfrikaans } from "./greetIn";
-
-export default function greet(person: Person) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const language_1 = require("./language");
+const greetIn_1 = require("./greetIn");
+const greetIn_2 = require("./greetIn");
+function greet(person) {
     if (!person.email) {
-        return `Hello, ${person.firstName} ${person.lastName} we can't contact you.`
+        return `Hello, ${person.firstName} ${person.lastName} we can't contact you.`;
     }
-     else return `Hello, ${person.firstName} ${person.lastName} we will be in touch at ${person.email}`;
-  }
-
+    else
+        return `Hello, ${person.firstName} ${person.lastName} we will be in touch at ${person.email}`;
+}
+exports.default = greet;
 // export function greet(name: string, chosenLanguage: language) {
 //     let greetIn : GreetIn = new GreetInEnglish();
 //     if (chosenLanguage === language.afr) {
@@ -20,24 +19,18 @@ export default function greet(person: Person) {
 //     if (chosenLanguage === language.xhosa) {
 //         greetIn = new GreetInXhosa();
 //     }
-    
 //     return greetIn.greet(name);
 // }
-
-let theGreetInMap : Map<language, GreetIn>  = new Map();
-
-theGreetInMap.set(language.eng, new GreetInEnglish());
-theGreetInMap.set(language.afr, new GreetInAfrikaans());
+let theGreetInMap = new Map();
+theGreetInMap.set(language_1.language.eng, new greetIn_1.GreetInEnglish());
+theGreetInMap.set(language_1.language.afr, new greetIn_2.GreetInAfrikaans());
 // theGreetInMap.set(language.xhosa, new GreetInXhosa());
-
 // export class Greeter {
 //      create a Map that has a languages enum as a key and a GreetIn interface instance as a value
 //     private greetLanguages:Map<language, GreetIn>
-
 //     constructor(greetLanguages:Map<language, GreetIn>){
 //         this.greetLanguages = greetLanguages;
 //     }
-    
 //     greet(name: string, chosenLanguage:language) {
 //         let greetIn = this.greetLanguages.get(chosenLanguage);
 //         if (greetIn) {
